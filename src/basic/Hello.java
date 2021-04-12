0f1519fe86b2fbf8215767c711f2824da8f190fb
seunghyunlee <kailyyy02@gmail.com>
2021-04-12 15:17:11 +0900

    Hello updated

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 //    주석(코드에 대한 설명, 이해하기 쉽게 붙이고 싶은 내용)을 처리
 	      System.out.println("Hello");
+	      System.out.println("안녕하세요");
+	      System.out.println("반갑습니다.");
 	}
 }
