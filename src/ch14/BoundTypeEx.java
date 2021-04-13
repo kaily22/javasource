5a3cc9873cb706d12c4469bdaba55e7cd62ba7cd
seunghyunlee <kailyyy02@gmail.com>
2021-04-13 14:53:02 +0900

    Generic 확인

---
+++ src/ch14/BoundTypeEx.java
@@ -0,0 +1,15 @@
+package ch14;
+
+public class BoundTypeEx {
+
+   public static void main(String[] args) {
+//      int result=BoundType.compare("a","b");
+      
+      int result=BoundType.compare(3.14, 4.15);
+      System.out.println(result);
+      
+      
+      
+   }
+
+}
---
+++ src/ch14/BoxEx3.java
@@ -0,0 +1,22 @@
+package ch14;
+
+public class BoxEx3 {
+
+	public static void main(String[] args) {
+
+		Box3<Fruit> fruitBox = new Box3<Fruit>();
+		Box3<Apple> appleBox = new Box3<Apple>();
+		Box3<Toy> toyBox = new Box3<Toy>();
+		
+		
+		fruitBox.add(new Fruit());
+		fruitBox.add(new Apple());
+		fruitBox.add(new Grape());
+		fruitBox.add(new Apple());
+		
+		appleBox.add(new Apple());
+		appleBox.add(null);
+		
+		toyBox.add(new Toy());
+	}
+}
